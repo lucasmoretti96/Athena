bd323783cb27e2780c93025510953e01a3906e2f
Aluno <Aluno@Digital.net>
2018-10-17 21:27:39 -0300

    Aplicação da arquitetura base do projeto

---
+++ Athena/src/main/java/br/com/olimpia/athena/Main.java
@@ -0,0 +1,13 @@
+package br.com.olimpia.athena;
+
+import br.com.olimpia.athena.handlers.Processor;
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+        
+        Processor processador = new Processor();
+
+        System.out.println(processador.getName());    
+    }
+}
