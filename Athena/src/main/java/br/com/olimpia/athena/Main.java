04ed3d3d7e29ae3f25b04200d08ea7e7f898545c
Aluno Bandtec <github@alunos.bandtec.com.br>
2018-10-29 21:23:11 -0200

    Trazendo informações de HD detalhadas e Processos

--- Athena/src/main/java/br/com/olimpia/athena/Main.java
+++ Athena/src/main/java/br/com/olimpia/athena/Main.java
@@ -1,12 +1,14 @@
 package br.com.olimpia.athena;
 
+import static Interface.IController.hal;
+import static Interface.IController.os;
 import br.com.olimpia.athena.handler.Computer;
-
 import java.util.Arrays;
+
 import oshi.util.FormatUtil;
 
 public class Main{
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         Computer info = new Computer();
         System.out.println("----Processador-------");
         System.out.println("Marca: "+info.getCpuName());
@@ -19,7 +21,14 @@ public class Main{
         System.out.println("Memória RAM Disponível: "+FormatUtil.formatBytesDecimal(info.getRamAvailable()));
         System.out.println("----HD------------");
         System.out.println("Memória Total do HD: "+FormatUtil.formatBytesDecimal(info.getHDTotal()));
+        System.out.println("Informações Gerais HD: ");
+        System.out.println(Arrays.toString(info.getHDFilesStoresSizes()));
         System.out.println("----Sistema Operacional----------");
         System.out.println("Tempo de Uso da Máquina: "+FormatUtil.formatElapsedSecs(info.getComputerUsageTime()));
+        System.out.println("-------Rede-----------------------");
+        System.out.println("IP da máquina: "+info.getComputerIpAddress());
+        System.out.println("--------Processos------------------");
+        System.out.println("Processos sendo executados na máquina: ");
+        System.out.println(info.printProcesses(os, hal.getMemory()));
     }
 }
--- Athena/src/main/java/br/com/olimpia/athena/handler/Computer.java
+++ Athena/src/main/java/br/com/olimpia/athena/handler/Computer.java
@@ -1,31 +1,28 @@
 package br.com.olimpia.athena.handler;
 
-import java.util.Arrays;
 import java.util.List;
-import oshi.SystemInfo;
+import java.util.ArrayList;
+import java.util.Arrays;
 import oshi.hardware.GlobalMemory;
-import oshi.hardware.HardwareAbstractionLayer;
+import oshi.hardware.HWDiskStore;
+import oshi.software.os.OSFileStore;
 import oshi.software.os.OSProcess;
 import oshi.software.os.OperatingSystem;
 import oshi.software.os.OperatingSystem.ProcessSort;
 import oshi.util.FormatUtil;
-import java.io.FileWriter;
-import java.io.File;
-import java.io.IOException;
-import java.text.DecimalFormat;
-import java.util.ArrayList;
-import oshi.hardware.HWDiskStore;
 
 public class Computer implements Interface.IController {
 
     private double cpuUsagePorcentage;
     private String cpuName;
+    private String computerIpAddress;
     private double cpuTemperature;
     private long ramTotal;
     private long ramUsage;
     private long ramAvailable;
     private long hdTotal;
     private long computerUsageTime;
+    private OSFileStore[] hdsDetails;
 
     public double getCpuUsagePorcentage() {
         cpuUsagePorcentage = hal.getProcessor().getSystemCpuLoadBetweenTicks();
@@ -76,5 +73,48 @@ public class Computer implements Interface.IController {
     public long getComputerUsageTime(){
         computerUsageTime = hal.getProcessor().getSystemUptime();
         return computerUsageTime;
+    }
+    
+    public String getComputerIpAddress(){
+        computerIpAddress = si.getOperatingSystem().getNetworkParams().getIpv4DefaultGateway();
+        return computerIpAddress;
+    }
+    
+    public OSFileStore[] getHDFilesStoresSizes(){
+        hdsDetails = os.getFileSystem().getFileStores();
+        
+        for (OSFileStore fs : hdsDetails) {
+            long usable = fs.getUsableSpace();
+            long total = fs.getTotalSpace();
+            System.out.format(" %s (%s) [%s] %s of %s free (%.1f%%) \n",
+
+                    fs.getName(), fs.getDescription().isEmpty() ? "file system" : fs.getDescription(), fs.getType(),
+
+                    FormatUtil.formatBytes(usable), FormatUtil.formatBytes(fs.getTotalSpace()), 100d * usable / total);
+    }
+        return hdsDetails;
+}
+    public List<OSProcess> printProcesses(OperatingSystem os, GlobalMemory memory) {
+        OSProcess p = new OSProcess();
+
+        System.out.println("Processes: " + os.getProcessCount() + ", Threads: " + os.getThreadCount());
+
+        List<OSProcess> procs = Arrays.asList(os.getProcesses(5, ProcessSort.CPU));
+
+        System.out.println("   PID  %CPU %MEM       VSZ       RSS Name");
+
+        for (int i = 0; i < procs.size() && i < 5; i++) {
+
+            p = procs.get(i);
+
+            System.out.format(" %5d %5.1f %4.1f %9s %9s %s%n", p.getProcessID(),
+
+                    100d * (p.getKernelTime() + p.getUserTime()) / p.getUpTime(),
+
+                    100d * p.getResidentSetSize() / memory.getTotal(), FormatUtil.formatBytes(p.getVirtualSize()),
+
+                    FormatUtil.formatBytes(p.getResidentSetSize()), p.getName());
+        }
+        return (List<OSProcess>) p;
     }
 }
